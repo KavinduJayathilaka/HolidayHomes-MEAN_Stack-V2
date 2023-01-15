4a0376ac3c0726ba02fbbcd68c3482f192ad7b07
KavinduJayathilaka <70713220+KavinduJayathilaka@users.noreply.github.com>
2023-01-15 15:38:05 +0530

    step 08 - pipes added for upper case and lower case latters

--- HolidayHomes-Rental-App/src/app/rental/rental.module.ts
+++ HolidayHomes-Rental-App/src/app/rental/rental.module.ts
@@ -6,6 +6,8 @@ import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalComponent } from './rental.component';
 import { CommonModule } from '@angular/common';
 import { RentalCardComponent } from '../shared/rental-card/rental-card.component';
+import { UppercasePipe, FirstUpperLetterPipe } from '../shared/pipes/uppercase.pipe';
+import { HighlightDirective } from '../shared/directives/custom.directive';
 
 import { RentalService } from './shared/rental.service';
 
@@ -29,7 +31,10 @@ const routes: Routes = [
     RentalDetailComponent,
     RentalListComponent,
     RentalComponent,
-    RentalCardComponent
+    RentalCardComponent,
+    UppercasePipe,
+    FirstUpperLetterPipe,
+    HighlightDirective
   ],
   imports: [
     RouterModule.forChild(routes),
