1bbb5d9b7968cbd32738eaf6107cc1b774a72af5
KavinduJayathilaka <70713220+KavinduJayathilaka@users.noreply.github.com>
2023-01-16 23:30:18 +0530

    step 17 - JWT and Middlewares

--- HolidayHomes-Rental-App/server/config/dev.js
+++ HolidayHomes-Rental-App/server/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-    DB_URI: 'mongodb+srv://HolidayHomes-Rentals:Kavicreed@cluster0.9da3uqp.mongodb.net/HolidayHomes-Rentals-DB?retryWrites=true&w=majority'
+    DB_URI: 'mongodb+srv://HolidayHomes-Rentals:Kavicreed@cluster0.9da3uqp.mongodb.net/HolidayHomes-Rentals-DB?retryWrites=true&w=majority',
+    JWT_SECRET: 'ad8a?897sad!'
 }
--- HolidayHomes-Rental-App/server/models/user.js
+++ HolidayHomes-Rental-App/server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -23,5 +24,18 @@ const userSchema = new Schema({
         required: 'Password is required!'
     }
 })
+userSchema.methods.hasSamePassword = function (providedPassword) {
+    return bcrypt.compareSync(providedPassword, this.password)
+}
 
+userSchema.pre('save', function (next) {
+    const user = this;
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            user.password = hash;
+            next();
+        })
+    })
+})
 module.exports = mongoose.model('User', userSchema);
