dcbefa3e101a35659c76e5f1e8c848bed2b0550d
KavinduJayathilaka <70713220+KavinduJayathilaka@users.noreply.github.com>
2023-01-17 22:38:24 +0530

    step 20 - logout function fixed !

--- HolidayHomes-Rental-App/src/app/auth/shared/auth.service.ts
+++ HolidayHomes-Rental-App/src/app/auth/shared/auth.service.ts
@@ -4,13 +4,33 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { exctractApiError } from 'src/app/shared/helpers/functions';
+import { JwtHelperService } from "@auth0/angular-jwt";
+
+import * as moment from 'moment';
+
+const jwt = new JwtHelperService();
+
+class DecodedToken {
+  exp: number = 0;
+  username: string = '';
+  userId: string = '';
+}
+
+
+
+// '16:36' < '18:36' - not expired!
+// '16:36' < '15:36' - expired!
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient){}
+  private decodedToken: DecodedToken;
+
+  constructor(private http: HttpClient){
+    this.decodedToken = new DecodedToken();
+  }
 
   //'/api/v1/users/register'
   register(formData: RegisterForm): Observable<any> {
@@ -28,15 +48,47 @@ export class AuthService {
       .post('/api/v1/users/login', formData)
       .pipe(
         map((token: any) => {
-          this.saveToken(token);
+          this.saveToken(token)
           return token;
         }),
         catchError((resError: HttpErrorResponse) => 
           throwError(exctractApiError(resError))
         )
     )}
+  logout() {
+      localStorage.removeItem('bwm_auth_token');
+      this.decodedToken = new DecodedToken();
+    }
+  checkAuthentication(): boolean {
+      const authToken = localStorage.getItem('bwm_auth_token');
+      if (!authToken) { return false; }
+  
+      const decodedToken = jwt.decodeToken(authToken);
+      if (!decodedToken) { return false; }
+  
+      this.decodedToken = decodedToken;
+      return true;
+    }  
 
-  private saveToken(token) {
-    alert('I am saving token!');
+  private saveToken(token: string): string | null {
+    const decodedToken = jwt.decodeToken(token);
+    if (!decodedToken) { return null; }
+
+    this.decodedToken = decodedToken;
+
+    localStorage.setItem('app_auth_token', token);
+    return token;
+  }
+
+  get isAuthenticated(): boolean {
+    return moment().isBefore(this.expiration)
+  }
+
+  get username(): string {
+    return this.decodedToken.username;
+  }
+
+  private get expiration() {
+    return moment.unix(this.decodedToken.exp);
   }
 }
