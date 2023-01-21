318baac604d7d20fd1173621accbd0a5cc18e1ea
KavinduJayathilaka <70713220+KavinduJayathilaka@users.noreply.github.com>
2023-01-22 02:33:20 +0530

    step 38 - websocket partially added!

--- HolidayHomes-Rental-App/server/index.js
+++ HolidayHomes-Rental-App/server/index.js
@@ -17,6 +17,7 @@ const User = require('./models/user');
 const booking = require('./models/booking');
 
 const app = express();
+const http = require('http').createServer(app)
 const PORT = process.env.PORT || 3001;
 
 mongoose.connect(config.DB_URI, {
@@ -29,10 +30,14 @@ mongoose.connect(config.DB_URI, {
 // Middleware
 app.use(bodyParser.json());
 app.use(provideErrorHandler);
+app.use(express.static(__dirname + '/public'))
 
 app.get('/api/v1/secret', onlyAuthUser, (req, res) => {
     const user = res.locals.user
     return res.json({ message: `Super secret message to: ${user.username}` })
+})
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
 })
 
 // Api Routes
@@ -40,6 +45,18 @@ app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', usersRoutes);
 app.use('/api/v1/bookings', bookingRoutes);
 
+// Socket 
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+
+})
+
 app.listen(PORT, () => {
     console.log('Server is up and running on port: ', PORT);
+    console.log('Connected...')
 })
