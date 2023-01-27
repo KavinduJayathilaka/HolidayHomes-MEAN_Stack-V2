3e910458aa23366c8a4182ecb5ddd8ac680d2d54
KavinduJayathilaka <70713220+KavinduJayathilaka@users.noreply.github.com>
2023-01-27 21:12:23 +0530

    step 54 - editable text area not working

--- HolidayHomes-Rental-App/src/app/shared/modules/editable/editable.module.ts
+++ HolidayHomes-Rental-App/src/app/shared/modules/editable/editable.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditableInputComponent } from './editable-input/editable-input.component';
 import { FormsModule } from '@angular/forms';
+import { EditableTextareaComponent } from './editable-textarea/editable-textarea.component';
 
 @NgModule({
   declarations: [
-    EditableInputComponent
+    EditableInputComponent,
+    EditableTextareaComponent
   ],
   exports: [
     EditableInputComponent
